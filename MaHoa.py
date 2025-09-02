0be2bab9722f584b1114439bad834c248dd83b34
HOÀNG MINH NGỌC <hoangminhngoc@192.168.2.8>
2025-09-02 14:06:42 +0700

    Add Caesar cipher encryption

---
+++ MaHoa.py
@@ -0,0 +1,23 @@
+def caesar_encrypt(text, key, alphabet):
+    key = key % 26 
+    encrypted = ""
+
+    text = text.upper()
+
+    for char in text:
+        if char in alphabet:
+            idx = alphabet.index(char)  
+            new_idx = (idx + key) % 26 
+            encrypted += alphabet[new_idx] 
+        else:
+            encrypted += char 
+    return encrypted
+
+plaintext = "HOANG THI MiNH NGOC"
+key = 35
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+
+result = caesar_encrypt(plaintext, key, alphabet)
+
+print("Văn bản gốc  :", plaintext)
+print("Mã hóa       :", result) 
